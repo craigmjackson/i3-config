780cd95c42cc42defb2586865761d65d696e0b2d
Craig Jackson <craigmjackson@gmail.com>
2026-03-27 23:06:47 -0700

    Fixed displays to handle portable and fixed setups

--- setup_displays.sh
+++ setup_displays.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-# sleep 5
+xrandr --query | grep "HDMI-1-0 connected" >/dev/null 2>&1
+if [ "$?" != "0" ]; then
+	exit 0
+fi
 xrandr --output eDP-1 --off --output HDMI-1-0 --auto --output DVI-I-3-2 --auto --output DVI-I-2-1 --auto
-# sleep 5
-# xrandr --output DVI-I-3-2 --auto
-# sleep 5
-# xrandr --output HDMI-1-0 --pos 1808x320 --output DVI-I-3-2 --left-of HDMI-1-0 --rotate right --pos 0x0
-# sleep 5
-# xrandr --output DVI-I-2-1 --auto
 xrandr --output HDMI-1-0 --pos 1808x320 --output DVI-I-3-2 --left-of HDMI-1-0 --rotate right --pos 0x0 --output DVI-I-2-1 --right-of HDMI-1-0 --pos 4920x320
